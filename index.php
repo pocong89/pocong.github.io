b96a27b932c7a6dd35729d186d5ca843482d90d8
pocong89 <138242532+pocong89@users.noreply.github.com>
2023-07-01 02:09:28 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+<stripe-pricing-table pricing-table-id="prctbl_1NOCTBAFvQFqvNkqWVnC4Yzf"
+publishable-key="pk_live_51NOBv6AFvQFqvNkqCTa8w6lEYhzf2TsYcrp9xRLoge21e1jMFvAYt0ratC9O6iX2cvtAujs7ebXmNVYoPWl5CzqX00dFNUZDi5">
+</stripe-pricing-table>
