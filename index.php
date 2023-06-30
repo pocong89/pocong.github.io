6e33dab56aebedeac61d57ade3b2084a52e41543
pocong89 <138242532+pocong89@users.noreply.github.com>
2023-07-01 02:16:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-<stripe-pricing-table pricing-table-id="prctbl_1NOCTBAFvQFqvNkqWVnC4Yzf"
-publishable-key="pk_live_51NOBv6AFvQFqvNkqCTa8w6lEYhzf2TsYcrp9xRLoge21e1jMFvAYt0ratC9O6iX2cvtAujs7ebXmNVYoPWl5CzqX00dFNUZDi5">
-</stripe-pricing-table>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tets</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
